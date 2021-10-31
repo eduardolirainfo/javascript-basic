f3bca820f8984e1d0da38fa5f3b624703389f02b
eduardolirainfo <eduardolirainfo@gmail.com>
2021-10-31 18:17:42 -0300

    Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,41 @@
+var nome = 'Eduardo'
+var idade = 35
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco)
+
+var sobreNome = 'Lira',
+  cidade = 'Rio de Janeiro'
+
+console.log(sobreNome, cidade)
+
+
+var semDefinir
+
+console.log(semDefinir)
+
+// Declarar uma variável com o seu nome
+var nome = 'Eduardo'
+
+// Declarar uma variável com a sua idade
+var idade = 35
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Lasanha'
+// Declarar 5 variáveis diferentes sem valores
+var bairro,
+  pais,
+  endereco,
+  rua,
+  time
+
+console.log(nome, idade, comidaFavorita)
