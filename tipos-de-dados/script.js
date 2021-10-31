c21ecfa865265fc6ad71a634b3a61f0d510dc907
eduardolirainfo <eduardolirainfo@gmail.com>
2021-10-31 19:35:46 -0300

    Tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,15 @@
+var nome = 'Eduardo'
+
+var idade = 35
+
+var time = null
+
+var simbolo = Symbol()
+
+console.log(typeof simbolo)
+
+
+var sobreNome = 'Lira'
+var nomeCompleto = nome + ' ' + sobreNome
+
+console.log(nomeCompleto)
